590479c6a1bad7d770be9eae987fa416ab08e3c2
tardis03 <111162303+argf013@users.noreply.github.com>
2023-01-31 02:32:13 +0700

    using cookies to save data todolist

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TodoList from './TodoList';
 function App() {
   return (
     <div className="App">
-      <h1>To do List App</h1>
+      <p style={{fontSize: '18px'}}>To do List App</p>
       <h4>Created by <a href='https://github.com/argf013' target='_blank' rel="noreferrer">Arfa</a></h4>
       <TodoList />
     </div>
